d5a85c5bc49deb2271e5d456342efdd6561508c6
bwyeag <bwyeag0001@outlook.com>
2025-06-16 16:50:39 +0800

    删除了旧的版本文件，开始文件的重构，不完全重写

--- src/inc/core/bl_constant.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef _BL_CONSTANT_HPP_FILE_
-#define _BL_CONSTANT_HPP_FILE_
-
-namespace BL {
-/// @brief 最大即时帧数目
-constexpr uint32_t MAX_FLIGHT_COUNT = 2;
-}  // namespace BL
-#endif  //!_BL_CONSTANT_HPP_FILE_
--- src/inc/core/bl_shader.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef _BL_SHADER_HPP_FILE_
-#define _BL_SHADER_HPP_FILE_
-
-namespace BL
-{
-    
-} // namespace BL
-#endif //!_BL_SHADER_HPP_FILE_
