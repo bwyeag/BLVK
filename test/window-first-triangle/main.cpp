fe8bbcb3cce1770e57be430efcff9cf8b68cf9d6
bwyeag <bwyeag0001@outlook.com>
2025-07-05 20:18:10 +0800

    modified file structure to include more test subprograms

---
+++ test/window-first-triangle/main.cpp
@@ -0,0 +1,80 @@
+#include "bl_rendering_loop.hpp"
+#include "bl_vktypes.hpp"
+#include "render_section.hpp"
+#include <bl_contexts2.hpp>
+#include <bl_debug_utils.hpp>
+#include <cstdint>
+using namespace BLT;
+WindowContext<WindowContextBase_glfw> window_context;
+ContextBase context;
+WindowRenderCtx<WindowContextBase_glfw> window_render_ctx;
+RenderSection<WindowContextBase_glfw> window_render_sect;
+debug::FpsTitle<WindowContextBase_glfw> debug_FpsTitle;
+const char *s_LogType = "Test";
+int main() {
+  print_log(s_LogType, "Start Program");
+  {
+    using wState = WindowCreateState;
+    InstanceCreateInfo ci_inst{.m_pAppName = "test",
+                               .m_AppVersion = VK_MAKE_API_VERSION(0, 0, 1, 0),
+                               .m_MinApiVersion = VK_API_VERSION_1_3,
+                               .m_isDebuging = true};
+    context.create_instance(ci_inst);
+    ContextTraits::set_current_context(context);
+    WindowCreateInfo_glfw ci_wctx{
+        .m_InitState = wState(wState::specified | wState::use_primary_monitor |
+                              wState::decorated | wState::resizable),
+        .m_InitSizeX = 800,
+        .m_InitSizeY = 600,
+        .m_InitPosX = 100,
+        .m_InitPosY = 100,
+        .m_InitTitle = "Test"};
+    window_context.create_base(ci_wctx);
+    window_context.create_surface(); // -> get_surface
+    DeviceCreateInfo ci_devi{};
+    auto surfaces = window_context.get_surface();
+    context.create_device(ci_devi, std::span(&surfaces, 1));
+    SwapchainCreateInfo ci_swch{};
+    window_context.create(ci_swch);
+
+    debug_FpsTitle.init(window_context, context);
+
+    WindowRenderCtxCreateInfo ci_wrctx{.m_pWindowCtx = &window_context,
+                                       .m_StagesNum = 1,
+                                       .m_Flags = WindowRenderCtxFlagBits(0)};
+    window_render_ctx.create(ci_wrctx);
+    window_render_sect.create(window_context);
+  }
+  print_log(s_LogType, "Finish Initialization.");
+  bool should_end = false;
+  VkClearValue clearColor = {.color = {{1.f, 0.f, 0.f, 1.f}}};
+  while (!glfwWindowShouldClose(window_context.m_pWindow) && !should_end) {
+    window_render_ctx.begin(
+        VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT,
+        [clearColor](VkCommandBuffer buf, uint32_t i) {
+          VkRenderPassBeginInfo begin_info{
+              .sType = VK_STRUCTURE_TYPE_RENDER_PASS_BEGIN_INFO,
+              .renderPass = window_render_sect.m_RenderPass,
+              .framebuffer = window_render_sect.m_Framebuffers[i],
+              .renderArea = {{},
+                             window_context.m_SwapchainCreateInfo.imageExtent},
+              .clearValueCount = 1,
+              .pClearValues = &clearColor};
+          window_render_sect.m_RenderPass.cmd_begin(buf, begin_info);
+          window_render_sect.m_RenderPass.cmd_end(buf);
+        },
+        true);
+    window_render_ctx.end_and_present(
+        VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT);
+    glfwPollEvents();
+    context.update();
+  }
+  print_log(s_LogType, "Program End.");
+  {
+    window_render_sect.cleanup();
+    window_render_ctx.cleanup();
+    window_context.cleanup();
+    context.cleanup();
+  }
+  print_log(s_LogType, "End Program");
+}
---
+++ test/window-first-triangle/render_section.hpp
@@ -0,0 +1,80 @@
+#include "bl_contexts2.hpp"
+#include <bl_vktypes.hpp>
+#include <cstddef>
+#include <memory>
+using namespace BLT;
+
+template <typename BaseCtx, typename _Ctx = ContextTraits>
+struct RenderSection {
+  WindowContext<BaseCtx, _Ctx> *m_pWindowCtx;
+  RenderPass<_Ctx> m_RenderPass;
+  std::unique_ptr<Framebuffer<_Ctx>[]> m_Framebuffers;
+
+  void create(WindowContext<BaseCtx, _Ctx> &window_ctx) {
+    m_pWindowCtx = &window_ctx;
+    // 对渲染使用的图像附件的描述
+    VkAttachmentDescription attachment_description = {
+        .format = window_ctx.m_SwapchainCreateInfo.imageFormat,
+        .samples = VK_SAMPLE_COUNT_1_BIT,
+        .loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR,
+        .storeOp = VK_ATTACHMENT_STORE_OP_STORE,
+        .initialLayout = VK_IMAGE_LAYOUT_UNDEFINED,
+        .finalLayout = VK_IMAGE_LAYOUT_PRESENT_SRC_KHR};
+    // 渲染使用的渲染阶段
+    VkAttachmentReference attachment_reference = {
+        0, VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL};
+    VkSubpassDescription subpass_description = {
+        .pipelineBindPoint = VK_PIPELINE_BIND_POINT_GRAPHICS,
+        .colorAttachmentCount = 1,
+        .pColorAttachments = &attachment_reference};
+    VkSubpassDependency subpass_dependency = {
+        .srcSubpass = VK_SUBPASS_EXTERNAL,
+        .dstSubpass = 0,
+        .srcStageMask = VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT,
+        .dstStageMask =
+            VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT, // 不早于提交命令缓冲区时等待semaphore对应的waitDstStageMask
+        .srcAccessMask = 0,
+        .dstAccessMask = VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT,
+        .dependencyFlags = VK_DEPENDENCY_BY_REGION_BIT};
+    VkRenderPassCreateInfo render_pass_ci = {
+        .attachmentCount = 1,
+        .pAttachments = &attachment_description,
+        .subpassCount = 1,
+        .pSubpasses = &subpass_description,
+        .dependencyCount = 1,
+        .pDependencies = &subpass_dependency};
+    m_RenderPass.create(render_pass_ci);
+
+    auto CreateFramebuffers =
+        [this](WindowContext<WindowContextBase_glfw, _Ctx> *_) {
+          auto &window_ctx = *m_pWindowCtx;
+          VkExtent2D &windowSize = window_ctx.m_SwapchainCreateInfo.imageExtent;
+          m_Framebuffers = std::make_unique<Framebuffer<_Ctx>[]>(
+              window_ctx.m_SwapchainImages.size());
+          VkFramebufferCreateInfo framebufferCreateInfo = {
+              .renderPass = m_RenderPass,
+              .attachmentCount = 1,
+              .width = windowSize.width,
+              .height = windowSize.height,
+              .layers = 1};
+          for (size_t i = 0; i < window_ctx.m_SwapchainImages.size(); i++) {
+            VkImageView attachment = window_ctx.m_SwapchainImageViews[i];
+            framebufferCreateInfo.pAttachments = &attachment;
+            m_Framebuffers[i].create(framebufferCreateInfo);
+          }
+        };
+    auto DestroyFramebuffers =
+        [this](WindowContext<WindowContextBase_glfw, _Ctx> *_) {
+          m_Framebuffers.release();
+        };
+
+    window_ctx.m_CallbackSwapchainConstruct.insert(CreateFramebuffers);
+    window_ctx.m_CallbackSwapchainDestroy.insert(DestroyFramebuffers);
+    CreateFramebuffers(m_pWindowCtx);
+  }
+  void cleanup() noexcept {
+    m_pWindowCtx = nullptr;
+    m_RenderPass.destroy();
+    m_Framebuffers.release();
+  }
+};
